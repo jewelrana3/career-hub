20af62f2645c8f8806023c3a64493e312ab38cee
jewelrana3 <juyelrana7752@gmail.com>
2023-04-10 08:02:48 +0600

    job placement

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './Header.css'
 import { Link } from 'react-router-dom';
 
+
 const Header = () => {
     return (
-        <nav>
+        <div>
+            <nav className='header'>
             <ul className='d-flex justify-content-around align-items-center'>
                 <h3>Project Hub</h3>
                 <div className='header-tag'>
@@ -16,6 +18,12 @@ const Header = () => {
                 <button className='apply-button'>Apply Now</button>
             </ul>
         </nav>
+
+        {/* Job Category List */}
+        <div>
+            <h1>Job Category List</h1>
+        </div>
+        </div>
     );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import images from '../../images/men.png'
 import './Home.css'
 
+
 const Home = () => {
     return (
+           
         <div className='home d-flex justify-content-center'>
             <div className='home-info'>
                 <h1>One Step </h1>
                 <h1>Closer To Your</h1> 
-                <h1>Dream Job</h1>
+                <h1><span className='dream'>Dream Job</span></h1>
                     <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
                 <button className='get-button'>Get Started</button>
             </div>
             <img src={images} alt="" />
         </div>
+        
     );
 };
 
