39c382e6396bff72496ffab63a47d86b3b957417
jewelrana3 <juyelrana7752@gmail.com>
2023-05-01 18:17:14 +0600

    details view btn

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -4,21 +4,27 @@ import './Details.css'
 const Details = ({ data }) => {
     const { remote, fulltime, location, jobTitle, salary, image } = data
     return (
-        <div className='d-flex p-2 rounded m-2' style={{ margin: '53px 0 0 20px',border:"1px solid" }}>
-            <img className='rounded' style={{ width: '240px', height: '240px', }} src={image} alt="" />
-            <div className='ms-4' style={{marginTop:'37px',}}>
-                <h4>{jobTitle}</h4>
-                <p>LLC Devolopment</p>
-                <div className=' d-flex ' >
-                    <p className='remote'>{remote}</p>
-                    <p className='remote' style={{ marginLeft: '14px' }}>{fulltime}</p>
-                </div>
-                <div className='d-flex'>
-                    <p>{location}</p>
-                    <p>{salary}</p>
+        <div className='d-flex justify-content-between p-2 rounded m-2' style={{ margin: '53px 0 0 20px', border: "1px solid" }}>
+            <div className='d-flex' >
+                <img className='rounded' style={{ width: '240px', height: '240px', }} src={image} alt="" />
+                <div>
+                    <h4>{jobTitle}</h4>
+                    <p>LLC Devolopment</p>
+                    <div className=' d-flex ' >
+                        <p className='remote'>{remote}</p>
+                        <p className='remote' style={{ marginLeft: '14px' }}>{fulltime}</p>
+                    </div>
+                    <div className='d-flex'>
+                        <p>{location}</p>
+                        <p>{salary}</p>
+                    </div>
                 </div>
             </div>
+            <div className=''>
+                <button className='view-details '>View Details</button>
+            </div>
         </div>
+
     );
 };
 
